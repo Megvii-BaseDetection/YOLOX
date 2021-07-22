c98bd482ff3c0054b37004e890a04db4109882eb
nitin <mneonizer@gmail.com>
2021-07-22 20:56:30 +0530

    Fixed ONNX simplification bug

--- tools/export_onnx.py
+++ tools/export_onnx.py
@@ -8,6 +8,8 @@ from loguru import logger
 
 import torch
 from torch import nn
+import onnx
+from onnxsim import simplify
 
 from yolox.exp import get_exp
 from yolox.models.network_blocks import SiLU
@@ -19,8 +21,8 @@ def make_parser():
     parser.add_argument(
         "--output-name", type=str, default="yolox.onnx", help="output name of models"
     )
-    parser.add_argument("--input", default="images", type=str, help="input name of onnx model")
-    parser.add_argument("--output", default="output", type=str, help="output name of onnx model")
+    parser.add_argument("--input", default="images", type=str, help="input node name of onnx model")
+    parser.add_argument("--output", default="output", type=str, help="output node name of onnx model")
     parser.add_argument("-o", "--opset", default=11, type=int, help="onnx opset version")
     parser.add_argument("--no-onnxsim", action="store_true", help="use onnxsim or not")
 
@@ -61,8 +63,8 @@ def main():
     else:
         ckpt_file = args.ckpt
 
-    ckpt = torch.load(ckpt_file, map_location="cpu")
     # load the model state dict
+    ckpt = torch.load(ckpt_file, map_location="cpu")
 
     model.eval()
     if "model" in ckpt:
@@ -71,7 +73,7 @@ def main():
     model = replace_module(model, nn.SiLU, SiLU)
     model.head.decode_in_inference = False
 
-    logger.info("loaded checkpoint done.")
+    logger.info("loading checkpoint done.")
     dummy_input = torch.randn(1, 3, exp.test_size[0], exp.test_size[1])
     torch.onnx._export(
         model,
@@ -81,12 +83,15 @@ def main():
         output_names=[args.output],
         opset_version=args.opset,
     )
-    logger.info("generate onnx named {}".format(args.output_name))
+    logger.info("generated onnx model named {}".format(args.output_name))
 
     if not args.no_onnxsim:
         # use onnxsimplify to reduce reduent model.
-        os.system("python3 -m onnxsim {} {}".format(args.output_name, args.output_name))
-        logger.info("generate simplify onnx named {}".format(args.output_name))
+        onnx_model = onnx.load(args.output_name)  # load onnx model
+        model_simp, check = simplify(onnx_model)
+        assert check, "Simplified ONNX model could not be validated"
+        onnx.save(model_simp, args.output_name)
+        logger.info("generated simplified onnx model named {}".format(args.output_name))
 
 
 if __name__ == "__main__":
