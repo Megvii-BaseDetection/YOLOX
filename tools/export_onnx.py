a48d03091b04f05a81867696a146af0860cf937a
nitin <mneonizer@gmail.com>
2021-07-23 09:54:29 +0530

    Moved onnxsim to dedicated block

--- tools/export_onnx.py
+++ tools/export_onnx.py
@@ -8,8 +8,6 @@ from loguru import logger
 
 import torch
 from torch import nn
-import onnx
-from onnxsim import simplify
 
 from yolox.exp import get_exp
 from yolox.models.network_blocks import SiLU
@@ -18,30 +16,25 @@ from yolox.utils import replace_module
 
 def make_parser():
     parser = argparse.ArgumentParser("YOLOX onnx deploy")
-    parser.add_argument(
-        "--output-name", type=str, default="yolox.onnx", help="output name of models"
-    )
-    parser.add_argument("--input", default="images", type=str, help="input node name of onnx model")
-    parser.add_argument("--output", default="output", type=str, help="output node name of onnx model")
-    parser.add_argument("-o", "--opset", default=11, type=int, help="onnx opset version")
-    parser.add_argument("--no-onnxsim", action="store_true", help="use onnxsim or not")
-
-    parser.add_argument(
-        "-f",
-        "--exp_file",
-        default=None,
-        type=str,
-        help="expriment description file",
-    )
+    parser.add_argument("--output-name", type=str, default="yolox.onnx",
+                        help="output name of models")
+    parser.add_argument("--input", default="images", type=str,
+                        help="input node name of onnx model")
+    parser.add_argument("--output", default="output", type=str,
+                        help="output node name of onnx model")
+    parser.add_argument("-o", "--opset", default=11, type=int,
+                        help="onnx opset version")
+    parser.add_argument("--no-onnxsim", action="store_true",
+                        help="use onnxsim or not")
+    parser.add_argument("-f", "--exp_file", default=None, type=str,
+                        help="expriment description file",)
     parser.add_argument("-expn", "--experiment-name", type=str, default=None)
-    parser.add_argument("-n", "--name", type=str, default=None, help="model name")
-    parser.add_argument("-c", "--ckpt", default=None, type=str, help="ckpt path")
-    parser.add_argument(
-        "opts",
-        help="Modify config options using the command-line",
-        default=None,
-        nargs=argparse.REMAINDER,
-    )
+    parser.add_argument("-n", "--name", type=str, default=None,
+                        help="model name")
+    parser.add_argument("-c", "--ckpt", default=None, type=str,
+                        help="ckpt path")
+    parser.add_argument("opts", help="Modify config options using the command-line",
+                        default=None, nargs=argparse.REMAINDER,)
 
     return parser
 
@@ -86,8 +79,11 @@ def main():
     logger.info("generated onnx model named {}".format(args.output_name))
 
     if not args.no_onnxsim:
+        import onnx
+        from onnxsim import simplify
+
         # use onnxsimplify to reduce reduent model.
-        onnx_model = onnx.load(args.output_name)  # load onnx model
+        onnx_model = onnx.load(args.output_name)
         model_simp, check = simplify(onnx_model)
         assert check, "Simplified ONNX model could not be validated"
         onnx.save(model_simp, args.output_name)
