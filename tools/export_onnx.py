44d837d1a1af249bf1ed6927c61097a8e8ab96d7
Nitin Rai <mneonizer@gmail.com>
2021-07-26 16:58:14 +0530

    chore(tools): fix potential bug  and update docs  (#89)
    chore(tools): fix potential bug  and update docs

--- tools/export_onnx.py
+++ tools/export_onnx.py
@@ -16,30 +16,25 @@ from yolox.utils import replace_module
 
 def make_parser():
     parser = argparse.ArgumentParser("YOLOX onnx deploy")
-    parser.add_argument(
-        "--output-name", type=str, default="yolox.onnx", help="output name of models"
-    )
-    parser.add_argument("--input", default="images", type=str, help="input name of onnx model")
-    parser.add_argument("--output", default="output", type=str, help="output name of onnx model")
-    parser.add_argument("-o", "--opset", default=11, type=int, help="onnx opset version")
-    parser.add_argument("--no-onnxsim", action="store_true", help="use onnxsim or not")
-
-    parser.add_argument(
-        "-f",
-        "--exp_file",
-        default=None,
-        type=str,
-        help="expriment description file",
-    )
+    parser.add_argument("--output-name", type=str, default="yolox.onnx",
+                        help="output name of models")
+    parser.add_argument("--input", default="images", type=str,
+                        help="input node name of onnx model")
+    parser.add_argument("--output", default="output", type=str,
+                        help="output node name of onnx model")
+    parser.add_argument("-o", "--opset", default=11, type=int,
+                        help="onnx opset version")
+    parser.add_argument("--no-onnxsim", action="store_true",
+                        help="use onnxsim or not")
+    parser.add_argument("-f", "--exp_file", default=None, type=str,
+                        help="expriment description file",)
     parser.add_argument("-expn", "--experiment-name", type=str, default=None)
-    parser.add_argument("-n", "--name", type=str, default=None, help="model name")
-    parser.add_argument("-c", "--ckpt", default=None, type=str, help="ckpt path")
-    parser.add_argument(
-        "opts",
-        help="Modify config options using the command-line",
-        default=None,
-        nargs=argparse.REMAINDER,
-    )
+    parser.add_argument("-n", "--name", type=str, default=None,
+                        help="model name")
+    parser.add_argument("-c", "--ckpt", default=None, type=str,
+                        help="ckpt path")
+    parser.add_argument("opts", help="Modify config options using the command-line",
+                        default=None, nargs=argparse.REMAINDER,)
 
     return parser
 
@@ -61,8 +56,8 @@ def main():
     else:
         ckpt_file = args.ckpt
 
-    ckpt = torch.load(ckpt_file, map_location="cpu")
     # load the model state dict
+    ckpt = torch.load(ckpt_file, map_location="cpu")
 
     model.eval()
     if "model" in ckpt:
@@ -71,7 +66,7 @@ def main():
     model = replace_module(model, nn.SiLU, SiLU)
     model.head.decode_in_inference = False
 
-    logger.info("loaded checkpoint done.")
+    logger.info("loading checkpoint done.")
     dummy_input = torch.randn(1, 3, exp.test_size[0], exp.test_size[1])
     torch.onnx._export(
         model,
@@ -81,12 +76,18 @@ def main():
         output_names=[args.output],
         opset_version=args.opset,
     )
-    logger.info("generate onnx named {}".format(args.output_name))
+    logger.info("generated onnx model named {}".format(args.output_name))
 
     if not args.no_onnxsim:
+        import onnx
+        from onnxsim import simplify
+
         # use onnxsimplify to reduce reduent model.
-        os.system("python3 -m onnxsim {} {}".format(args.output_name, args.output_name))
-        logger.info("generate simplify onnx named {}".format(args.output_name))
+        onnx_model = onnx.load(args.output_name)
+        model_simp, check = simplify(onnx_model)
+        assert check, "Simplified ONNX model could not be validated"
+        onnx.save(model_simp, args.output_name)
+        logger.info("generated simplified onnx model named {}".format(args.output_name))
 
 
 if __name__ == "__main__":
