9dc0feb150e9763b95e1b2799bc725e4f23a2e7b
Ren Jin <jariof@foxmail.com>
2021-08-03 11:07:55 +0800

    Fix compile error in Windows environment (#274)

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ with open("yolox/__init__.py", "r") as f:
     ).group(1)
 
 
-with open("README.md", "r") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 
