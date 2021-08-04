2488d9debee58a27b31f42fd4ac85d3b952c60a4
pmj110119 <929041691@qq.com>
2021-08-04 16:35:26 +0800

    use amp optimizer

--- yolox/core/trainer.py
+++ yolox/core/trainer.py
@@ -137,7 +137,7 @@ class Trainer:
         self.optimizer = self.exp.get_optimizer(self.args.batch_size)
 
         if self.amp_training:
-            model, optimizer = amp.initialize(model, self.optimizer, opt_level="O1")
+            model, self.optimizer = amp.initialize(model, self.optimizer, opt_level="O1")
 
         # value of epoch will be set in `resume_train`
         model = self.resume_train(model)
