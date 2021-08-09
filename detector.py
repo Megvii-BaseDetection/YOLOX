f7f2ff18ceb7d70cd7ce8f0fb8323abaa68f7a3d
wangluming <ming845378603@126.com>
2021-08-09 10:52:52 +0800

    feat:finish YOLOX detect

--- detector.py
+++ detector.py
@@ -1,75 +1,254 @@
 import sys
+
 sys.path.insert(0, './YOLOX')
 import torch
-import time
 import cv2
-
+from loguru import logger
 from YOLOX.yolox.data.data_augment import preproc
 from YOLOX.yolox.data.datasets import COCO_CLASSES
-from YOLOX.yolox.exp.build import get_exp_by_name
+from yolox.exp import get_exp
 from YOLOX.yolox.utils import postprocess
-from utils.visualize import vis
+from yolox.utils import get_model_info, postprocess, vis
 
-
+import argparse
+import os
+import time
 
 COCO_MEAN = (0.485, 0.456, 0.406)
 COCO_STD = (0.229, 0.224, 0.225)
+IMAGE_EXT = [".jpg", ".jpeg", ".webp", ".bmp", ".png"]
 
 
+def make_parser():
+    model = 'yolox-s'
+    ckpt = 'model/yolox_s.pth'
+    video_or_image = "video"
+    video_path = "input/8th_20210615/radar_20210615_144541.mp4"
+    # video_or_image = "image"
+    # video_path = "YOLOX/assets/dog.jpg"
+    conf = 0.3
+    nms = 0.5
+    tsize = 640
+    device = "gpu"
+    save_result = True
+
+    parser = argparse.ArgumentParser("YOLOX Demo!")
+    parser.add_argument("--demo", default=video_or_image, help="demo type, eg. image video")
+    parser.add_argument("-n", "--name", type=str, default=model, help="model name")
+    parser.add_argument("--path", default=video_path, help="path to images or video")
+    parser.add_argument("--save_result",
+                        default=save_result,
+                        action="store_true",
+                        help="whether to save the inference result of image/video",
+                        )
+
+    # exp file
+    parser.add_argument("-c", "--ckpt", default=ckpt, type=str, help="ckpt for eval")
+    parser.add_argument("--device", default=device, type=str, help="can either be cpu or gpu", )
+    parser.add_argument("--conf", default=conf, type=float, help="test conf")
+    parser.add_argument("--nms", default=nms, type=float, help="test nms threshold")
+    parser.add_argument("--tsize", default=tsize, type=int, help="test img size")
+    parser.add_argument(
+        "-f",
+        "--exp_file",
+        default=None,
+        type=str,
+        help="pls input your expriment description file",
+    )
+    parser.add_argument(
+        "--fp16",
+        dest="fp16",
+        default=False,
+        action="store_true",
+        help="Adopting mix precision evaluating.",
+    )
+    parser.add_argument(
+        "--fuse",
+        dest="fuse",
+        default=False,
+        action="store_true",
+        help="Fuse conv and bn for testing.",
+    )
+    return parser
 
 
-class Detector():
+def args_analysis(args):
+    exp = get_exp(args.exp_file, args.name)
 
-    def __init__(self, model='yolox-s', ckpt='model/yolox_s.pth'):
-        super(Detector, self).__init__()
+    args.experiment_name = exp.exp_name
+
+    file_name = os.path.join(exp.output_dir, args.experiment_name)
+    os.makedirs(file_name, exist_ok=True)
+
+    if args.save_result:
+        vis_folder = os.path.join(file_name, "vis_res")
+        os.makedirs(vis_folder, exist_ok=True)
+
+    logger.info("Args: {}".format(args))
+
+    if args.conf is not None:
+        exp.test_conf = args.conf
+    if args.nms is not None:
+        exp.nmsthre = args.nms
+    if args.tsize is not None:
+        exp.test_size = (args.tsize, args.tsize)
+
+    model = exp.get_model()
+    logger.info("Model Summary: {}".format(get_model_info(model, exp.test_size)))
+
+    if args.device == "gpu":
+        model.cuda()
+    model.eval()
+
+    if args.ckpt is None:
+        ckpt_file = os.path.join(file_name, "best_ckpt.pth.tar")
+    else:
+        ckpt_file = args.ckpt
+    ckpt = torch.load(ckpt_file, map_location="cpu")
+    model.load_state_dict(ckpt["model"])
+    logger.info("loaded checkpoint done.")
+    decoder = None
+    return model, exp, decoder, vis_folder
 
 
+class Predictor(object):
+    def __init__(
+            self,
+            model,
+            exp,
+            cls_names=COCO_CLASSES,
+            decoder=None,
+            device="cpu",
+    ):
+        self.model = model
+        self.cls_names = cls_names
+        self.decoder = decoder
+        self.num_classes = exp.num_classes
+        self.confthre = exp.test_conf
+        self.nmsthre = exp.nmsthre
+        self.test_size = exp.test_size
+        self.device = device
+        self.rgb_means = COCO_MEAN
+        self.std = COCO_STD
 
-        self.device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+    def inference(self, img):
+        img_info = {"id": 0}
+        if isinstance(img, str):
+            img_info["file_name"] = os.path.basename(img)
+            img = cv2.imread(img)
+        else:
+            img_info["file_name"] = None
 
-        self.exp = get_exp_by_name(model)
-        self.test_size = self.exp.test_size  # TODO: 改成图片自适应大小
-        self.model = self.exp.get_model()
-        self.model.to(self.device)
-        self.model.eval()
-        checkpoint = torch.load(ckpt, map_location="cpu")
-        self.model.load_state_dict(checkpoint["model"])
+        height, width = img.shape[:2]
+        img_info["height"] = height
+        img_info["width"] = width
+        img_info["raw_img"] = img
 
-
-
-    def detect(self, raw_img, visual=True, conf=0.5):
-        info = {}
-        img, ratio = preproc(raw_img, self.test_size, COCO_MEAN, COCO_STD)
-        info['raw_img'] = raw_img
-        info['img'] = img
-
+        img, ratio = preproc(img, self.test_size, self.rgb_means, self.std)
+        img_info["ratio"] = ratio
         img = torch.from_numpy(img).unsqueeze(0)
-        img = img.to(self.device)
+        if self.device == "gpu":
+            img = img.cuda()
 
         with torch.no_grad():
+            t0 = time.time()
             outputs = self.model(img)
+            if self.decoder is not None:
+                outputs = self.decoder(outputs, dtype=outputs.type())
             outputs = postprocess(
-                outputs, self.exp.num_classes, self.exp.test_conf, self.exp.nmsthre  # TODO:用户可更改
-            )[0].cpu().numpy()
+                outputs, self.num_classes, self.confthre, self.nmsthre
+            )
+            logger.info("Infer time: {:.4f}s".format(time.time() - t0))
+        return outputs, img_info
 
-        info['boxes'] = outputs[:, 0:4]/ratio
-        info['scores'] = outputs[:, 4] * outputs[:, 5]
-        info['class_ids'] = outputs[:, 6]
-        info['box_nums'] = outputs.shape[0]
-        # 可视化绘图
-        # if visual:
-        #     info['visual'] = vis(info['raw_img'], info['boxes'], info['scores'], info['class_ids'], conf, COCO_CLASSES)
-        return info
+    def visual(self, output, img_info, cls_conf=0.35):
+        ratio = img_info["ratio"]
+        img = img_info["raw_img"]
+        if output is None:
+            return img
+        output = output.cpu()
+
+        bboxes = output[:, 0:4]
+
+        # preprocessing: resize
+        bboxes /= ratio
+
+        cls = output[:, 6]
+        scores = output[:, 4] * output[:, 5]
+
+        vis_res = vis(img, bboxes, scores, cls, cls_conf, self.cls_names)
+        return vis_res
 
 
+def get_image_list(path):
+    image_names = []
+    for maindir, subdir, file_name_list in os.walk(path):
+        for filename in file_name_list:
+            apath = os.path.join(maindir, filename)
+            ext = os.path.splitext(apath)[1]
+            if ext in IMAGE_EXT:
+                image_names.append(apath)
+    return image_names
 
 
+def image_demo(predictor, vis_folder, path, current_time, save_result):
+    if os.path.isdir(path):
+        files = get_image_list(path)
+    else:
+        files = [path]
+    files.sort()
+    for image_name in files:
+        outputs, img_info = predictor.inference(image_name)
+        result_image = predictor.visual(outputs[0], img_info, predictor.confthre)
+        if save_result:
+            save_folder = os.path.join(
+                vis_folder, time.strftime("%Y_%m_%d_%H_%M_%S", current_time)
+            )
+            os.makedirs(save_folder, exist_ok=True)
+            save_file_name = os.path.join(save_folder, os.path.basename(image_name))
+            logger.info("Saving detection result in {}".format(save_file_name))
+            cv2.imwrite(save_file_name, result_image)
+        ch = cv2.waitKey(0)
+        if ch == 27 or ch == ord("q") or ch == ord("Q"):
+            break
 
 
-if __name__=='__main__':
-    detector = Detector()
-    img = cv2.imread('dog.jpg')
-    out = detector.detect(img)
-    cv2.imshow("Detection", img)
-    cv2.waitKey(0)
+def imageflow_demo(predictor, vis_folder, current_time, args):
+    cap = cv2.VideoCapture(args.path)
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)  # float
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    save_folder = os.path.join(
+        vis_folder, time.strftime("%Y_%m_%d_%H_%M_%S", current_time)
+    )
+    os.makedirs(save_folder, exist_ok=True)
+    save_path = os.path.join(save_folder, args.path.split("/")[-1])
+    logger.info(f"video save_path is {save_path}")
+    vid_writer = cv2.VideoWriter(
+        save_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (int(width), int(height))
+    )
+    while True:
+        ret_val, frame = cap.read()
+        if ret_val:
+            outputs, img_info = predictor.inference(frame)
+            result_frame = predictor.visual(outputs[0], img_info, predictor.confthre)
+            cv2.imshow("Detecting", result_frame)
+            if args.save_result:
+                vid_writer.write(result_frame)
+            ch = cv2.waitKey(1)
+            if ch == 27 or ch == ord("q") or ch == ord("Q"):
+                break
+        else:
+            break
     cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    args = make_parser().parse_args()
+    model, exp, decoder, vis_folder = args_analysis(args)
+
+    predictor = Predictor(model, exp, COCO_CLASSES, decoder, args.device)
+    current_time = time.localtime()
+    if args.demo == "image":
+        image_demo(predictor, vis_folder, args.path, current_time, args.save_result)
+    elif args.demo == "video":
+        imageflow_demo(predictor, vis_folder, current_time, args)
