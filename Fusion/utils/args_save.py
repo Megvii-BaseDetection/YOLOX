fcbb68b29366f400e40d8d54df4b213575d717b2
wangluming <ming845378603@126.com>
2021-08-11 19:10:45 +0800

    feat:finish the fusion_in_camera parameter.

--- Fusion/utils/args_save.py
+++ Fusion/utils/args_save.py
@@ -8,3 +8,16 @@
 # y_in = np.linspace(0, 900, 10)
 # y_result = [22, 25, 32, 42, 55, 70, 100, 200, 470, 800]
 # y_new = np.linspace(0, 899, 900)
+
+
+# 20210615_144541_in_camera
+# out_t = [-3, 0, 0]  # tx，ty,tz
+# out_angle = [0, 0, 0]  # angle_x，angle_y,angle_z
+# in_matrix = [960, 540, 3500, 3000]  # cx,cy,fx,fy
+# car_height = -4
+# camera_height = 9  # 相机地面高度
+# camera_angle_a = 5  # 相机光抽和水平线夹角
+# y_in = [22, 25, 32, 42, 55, 70, 100, 200, 470, 800]
+# y_result = np.linspace(0, 850, 10)
+# y_new = np.linspace(22, 800, 779)
+
--- Fusion/utils/convert.py
+++ Fusion/utils/convert.py
@@ -3,9 +3,10 @@ from scipy import interpolate
 import numpy as np
 import pylab as pl
 
-out_t = [3, 0, 0]  # tx，ty,tz
+out_t = [-3, 0, 0]  # tx，ty,tz
 out_angle = [0, 0, 0]  # angle_x，angle_y,angle_z
 in_matrix = [960, 540, 3500, 3000]  # cx,cy,fx,fy
+
 car_height = -4
 camera_height = 9  # 相机地面高度
 camera_angle_a = 5  # 相机光抽和水平线夹角
@@ -45,18 +46,45 @@ def convert_to_uv(xyz_in_camera, intrinsics_array=in_matrix):
     """
     相机--uv
     """
-    s = xyz_in_camera[0, 2]
+    x = xyz_in_camera[0, 0]
+    y = xyz_in_camera[0, 1]
+    z = xyz_in_camera[0, 2]
+
     u0 = intrinsics_array[0]
     v0 = intrinsics_array[1]
     fx = intrinsics_array[2]
     fy = intrinsics_array[3]
-    intrinsics_matrix = np.mat([
-        [fx, 0, u0],
-        [0, fy, v0],
-        [0, 0, 1]
-    ])
-    radar_uv = np.dot(intrinsics_matrix, xyz_in_camera.T)
-    return radar_uv / s
+
+    y_in = [22, 25, 32, 42, 55, 70, 100, 200, 470, 800]
+    y_result = np.linspace(0, 850, 10)
+    y_new = np.linspace(22, 800, 779)
+    fy = interpolate.interp1d(y_in, y_result, kind="quadratic")
+    y_new_result = fy(y_new)
+
+    v = 2 * v0 - y_new_result[int(z)]
+
+    # v = fy * y / z + v0
+    u = fx * x / z + u0
+
+    return [u, v]
+
+
+# def convert_to_uv(xyz_in_camera, intrinsics_array=in_matrix):
+#     """
+#     相机--uv
+#     """
+#     s = xyz_in_camera[0, 2]
+#     u0 = intrinsics_array[0]
+#     v0 = intrinsics_array[1]
+#     fx = intrinsics_array[2]
+#     fy = intrinsics_array[3]
+#     intrinsics_matrix = np.mat([
+#         [fx, 0, u0],
+#         [0, fy, v0],
+#         [0, 0, 1]
+#     ])
+#     radar_uv = np.dot(intrinsics_matrix, xyz_in_camera.T)
+#     return radar_uv / s
 
 
 # def calculate_depth(rect_roi, intrinsics_array=in_matrix):
