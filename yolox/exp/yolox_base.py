947a02bc434ed95973d03ff01107afaa67ed41b9
Shacar Gluska <shacharg@hailo.ai>
2021-09-12 11:42:36 +0000

    Added leaky relu variants

--- yolox/exp/yolox_base.py
+++ yolox/exp/yolox_base.py
@@ -20,6 +20,7 @@ class Exp(BaseExp):
         self.num_classes = 80
         self.depth = 1.00
         self.width = 1.00
+        self.act = 'silu'
 
         # ---------------- dataloader config ---------------- #
         # set worker to 4 for shorter dataloader init time
@@ -80,8 +81,8 @@ class Exp(BaseExp):
 
         if getattr(self, "model", None) is None:
             in_channels = [256, 512, 1024]
-            backbone = YOLOPAFPN(self.depth, self.width, in_channels=in_channels)
-            head = YOLOXHead(self.num_classes, self.width, in_channels=in_channels)
+            backbone = YOLOPAFPN(self.depth, self.width, in_channels=in_channels, act=self.act)
+            head = YOLOXHead(self.num_classes, self.width, in_channels=in_channels, act=self.act)
             self.model = YOLOX(backbone, head)
 
         self.model.apply(init_yolo)
