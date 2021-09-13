5ae0ba2bb984d613d6f609dc77dfde37cde09fca
wwqgtxx <wwqgtxx@gmail.com>
2021-09-13 18:09:10 +0800

    support setting max batch size when convert to trt

--- tools/trt.py
+++ tools/trt.py
@@ -27,6 +27,7 @@ def make_parser():
         help="pls input your expriment description file",
     )
     parser.add_argument("-c", "--ckpt", default=None, type=str, help="ckpt path")
+    parser.add_argument("-b", '--batch', type=int, default=1, help='max batch size in detect')
     return parser
 
 
@@ -60,6 +61,7 @@ def main():
         fp16_mode=True,
         log_level=trt.Logger.INFO,
         max_workspace_size=(1 << 32),
+        max_batch_size=args.batch,
     )
     torch.save(model_trt.state_dict(), os.path.join(file_name, "model_trt.pth"))
     logger.info("Converted TensorRT model done.")
