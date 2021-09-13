47d996868109622144f6a44a59698218064780c0
Xa9aX ツ <mishradiganta91@gmail.com>
2021-09-13 21:36:03 +0530

    WIP

--- yolox/utils/wandb/log_dataset.py
+++ yolox/utils/wandb/log_dataset.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', type=str, default='data/coco128.yaml', help='data.yaml path')
     parser.add_argument('--single-cls', action='store_true', help='train as single-class dataset')
-    parser.add_argument('--project', type=str, default='YOLOv5', help='name of W&B Project')
+    parser.add_argument('--project', type=str, default='YOLOX', help='name of W&B Project')
     parser.add_argument('--entity', default=None, help='W&B entity')
     parser.add_argument('--name', type=str, default='log dataset', help='name of W&B run')
 
