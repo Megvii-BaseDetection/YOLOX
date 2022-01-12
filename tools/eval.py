25f116b3145db3f2808ffbe722677d301e34f40d
Feng Wang <wangfeng19950315@163.com>
2022-01-12 15:37:34 +0800

    fix(evaluator): fix attr bug in COCOEvaluator (#1067)

--- tools/eval.py
+++ tools/eval.py
@@ -143,7 +143,7 @@ def main(exp, args, num_gpu):
     logger.info("Model Structure:\n{}".format(str(model)))
 
     evaluator = exp.get_evaluator(args.batch_size, is_distributed, args.test, args.legacy)
-    evaluator.per_class_mAP = True
+    evaluator.per_class_AP = True
     evaluator.per_class_AR = True
 
     torch.cuda.set_device(rank)
