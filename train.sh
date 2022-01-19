a1c30db6c395116174db63eb0bb4f6ccdb60f050
bwz <549068446@qq.com>
2022-01-19 19:22:14 +0800

    modify train code class

---
+++ train.sh
@@ -0,0 +1,8 @@
+python tools/train.py \
+-f /home/ecnu-lzw/bwz/ocr-gy/YOLOX/myconfig/voc_ocr.py \
+-d 2 -b 64 --fp16 -o \
+-c /home/ecnu-lzw/bwz/ocr-gy/YOLOX/YOLOX_outputs/yolox_voc_s_2012/best_ckpt.pth \
+--cache
+
+# cd /home/ecnu-lzw/bwz/ocr-gy/YOLOX
+# ./train.sh
--- yolox/data/datasets/voc_classes.py
+++ yolox/data/datasets/voc_classes.py
@@ -3,25 +3,30 @@
 # Copyright (c) Megvii, Inc. and its affiliates.
 
 # VOC_CLASSES = ( '__background__', # always index 0
-VOC_CLASSES = (
-    "aeroplane",
-    "bicycle",
-    "bird",
-    "boat",
-    "bottle",
-    "bus",
-    "car",
-    "cat",
-    "chair",
-    "cow",
-    "diningtable",
-    "dog",
-    "horse",
-    "motorbike",
-    "person",
-    "pottedplant",
-    "sheep",
-    "sofa",
-    "train",
-    "tvmonitor",
-)
+# VOC_CLASSES = (
+#     "aeroplane",
+#     "bicycle",
+#     "bird",
+#     "boat",
+#     "bottle",
+#     "bus",
+#     "car",
+#     "cat",
+#     "chair",
+#     "cow",
+#     "diningtable",
+#     "dog",
+#     "horse",
+#     "motorbike",
+#     "person",
+#     "pottedplant",
+#     "sheep",
+#     "sofa",
+#     "train",
+#     "tvmonitor",
+# )
+
+VOC_CLASSES = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+           'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+           'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+           'u', 'v', 'w', 'x', 'y', 'z', '-', '&', 'box', '%')
