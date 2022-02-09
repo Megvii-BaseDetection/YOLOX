853c6f05634d1e3d0e66cf6506861e018aa62d1e
bwz <549068446@qq.com>
2022-02-09 16:38:22 +0800

    onnx inference stable

--- pth2onnx.sh
+++ pth2onnx.sh
@@ -1,7 +1,7 @@
 python tools/export_onnx.py \
--c /home/ecnu-lzw/bwz/ocr-gy/YOLOX/YOLOX_outputs/voc_ocr_2029/best_ckpt.pth \
--f /home/ecnu-lzw/bwz/ocr-gy/YOLOX/myconfig/voc_ocr_2029.py \
---output-name=yolox_s2029.onnx \
+-c /home/ecnu-lzw/bwz/ocr-gy/YOLOX/YOLOX_outputs/voc_ocr_2023/best_ckpt.pth \
+-f /home/ecnu-lzw/bwz/ocr-gy/YOLOX/myconfig/voc_ocr_2023.py \
+--output-name=yolox_s2023.onnx \
 --dynamic --no-onnxsim
 
 # /home/ecnu-lzw/bwz/ocr-gy/YOLOX/pth2onnx.sh
