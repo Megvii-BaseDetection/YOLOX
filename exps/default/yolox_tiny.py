57309c730d9f8c0dd1496706886ec00ddc8a11d2
Feng Ni <nemonameless@qq.com>
2022-02-18 18:44:07 +0800

    fix(exps): typo in tiny (#1129)
    fix(exps): typo in tiny

--- exps/default/yolox_tiny.py
+++ exps/default/yolox_tiny.py
@@ -12,7 +12,7 @@ class Exp(MyExp):
         super(Exp, self).__init__()
         self.depth = 0.33
         self.width = 0.375
-        self.input_scale = (416, 416)
+        self.input_size = (416, 416)
         self.mosaic_scale = (0.5, 1.5)
         self.random_size = (10, 20)
         self.test_size = (416, 416)
