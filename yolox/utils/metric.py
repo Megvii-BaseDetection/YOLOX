cdbf67af849070a0bc59c101665030f16549a8b4
Vyacheslav Eliseev <eliseev.1999.slava@mail.ru>
2022-02-21 01:35:09 +0300

    fix(utils): occupy_mem based on CUDA_VISIBLE_DEVICES

--- yolox/utils/metric.py
+++ yolox/utils/metric.py
@@ -24,6 +24,9 @@ def get_total_and_free_memory_in_Mb(cuda_device):
         "nvidia-smi --query-gpu=memory.total,memory.used --format=csv,nounits,noheader"
     )
     devices_info = devices_info_str.read().strip().split("\n")
+    if "CUDA_VISIBLE_DEVICES" in os.environ:
+        visible_devices = os.environ["CUDA_VISIBLE_DEVICES"].split(',')
+        cuda_device = int(visible_devices[cuda_device])
     total, used = devices_info[int(cuda_device)].split(",")
     return int(total), int(used)
 
