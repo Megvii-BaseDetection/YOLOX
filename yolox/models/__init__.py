d18f5e8202cd2d8480eff9263a6a4456a11d90cf
Feng Wang <wangfeng19950315@163.com>
2022-03-21 15:04:12 +0800

    feat(model): support hub load

--- yolox/models/__init__.py
+++ yolox/models/__init__.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 # Copyright (c) Megvii Inc. All rights reserved.
 
+from .build import *
 from .darknet import CSPDarknet, Darknet
 from .losses import IOUloss
 from .yolo_fpn import YOLOFPN
