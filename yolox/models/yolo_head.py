e1fc5d3d095c921a82fa6d09e1770bb711825302
Toshihiro Kobayashi <penkoba@gmail.com>
2022-04-18 15:27:35 +0900

    change operation order in cross-entropy preprocess

--- yolox/models/yolo_head.py
+++ yolox/models/yolo_head.py
@@ -487,11 +487,12 @@ class YOLOXHead(nn.Module):
 
         with torch.cuda.amp.autocast(enabled=False):
             cls_preds_ = (
-                cls_preds_.float().unsqueeze(0).repeat(num_gt, 1, 1).sigmoid_()
-                * obj_preds_.float().unsqueeze(0).repeat(num_gt, 1, 1).sigmoid_()
-            )
+                cls_preds_.float().unsqueeze(0).sigmoid_()
+                * obj_preds_.float().unsqueeze(0).sigmoid_()
+            ).sqrt_()
             pair_wise_cls_loss = F.binary_cross_entropy(
-                cls_preds_.sqrt_(), gt_cls_per_image, reduction="none"
+                cls_preds_.repeat(num_gt, 1, 1), gt_cls_per_image,
+                reduction="none"
             ).sum(-1)
         del cls_preds_
 
