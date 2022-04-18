1dee0f767a47f9b145dbef9e0f26d6eb54335c7a
Feng Wang <wangfeng19950315@163.com>
2022-04-18 14:11:58 +0800

    fix(setup): cpp extension (#1254)

--- yolox/data/datasets/coco.py
+++ yolox/data/datasets/coco.py
@@ -191,7 +191,7 @@ class COCODataset(Dataset):
         img_file = os.path.join(self.data_dir, self.name, file_name)
 
         img = cv2.imread(img_file)
-        assert img is not None
+        assert img is not None, f"file named {img_file} not found"
 
         return img
 
