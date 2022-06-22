e265af3c63e36af55ba0c759dc7e7cf3a2f76740
bottled.mind <nikolay.konovalchuk@mail.ru>
2022-06-22 11:43:12 +0300

    feat(hubconf): loading a custom model with `torch.hub.load`

--- hubconf.py
+++ hubconf.py
@@ -5,6 +5,8 @@
 Usage example:
     import torch
     model = torch.hub.load("Megvii-BaseDetection/YOLOX", "yolox_s")
+    model = torch.hub.load("Megvii-BaseDetection/YOLOX", "yolox_custom",
+                           exp_path="exp.py", ckpt_path="ckpt.pth")
 """
 dependencies = ["torch"]
 
@@ -16,4 +18,5 @@ from yolox.models import (  # isort:skip  # noqa: F401, E402
     yolox_l,
     yolox_x,
     yolov3,
+    yolox_custom
 )
