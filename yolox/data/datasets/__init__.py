0ae6ad6dc94e38ae11816efd524c9ca290500edd
lujulia <39236354+lujulia@users.noreply.github.com>
2022-07-09 22:34:06 +0800

    Update __init__.py

--- yolox/data/datasets/__init__.py
+++ yolox/data/datasets/__init__.py
@@ -6,4 +6,5 @@ from .coco import COCODataset
 from .coco_classes import COCO_CLASSES
 from .datasets_wrapper import ConcatDataset, Dataset, MixConcatDataset
 from .mosaicdetection import MosaicDetection
+from .voc_classes import VOC_CLASSES
 from .voc import VOCDetection
