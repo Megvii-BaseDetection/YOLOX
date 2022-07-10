79c33158e364543c6897da51e07e07b8dc583f3a
lujulia <39236354+lujulia@users.noreply.github.com>
2022-07-10 15:05:16 +0800

    Update yolox_voc_nano_adam.py

--- exps/example/yolox_pedestrian/voc_format/yolox_voc_nano_adam.py
+++ exps/example/yolox_pedestrian/voc_format/yolox_voc_nano_adam.py
@@ -14,7 +14,7 @@ from yolox.exp import Exp as MyExp
 class Exp(MyExp):
     def __init__(self):
         super(Exp, self).__init__()
-        self.num_classes = 20
+        self.num_classes = 1
         self.depth = 0.33
         self.width = 0.25
         self.input_size = (416, 416)
