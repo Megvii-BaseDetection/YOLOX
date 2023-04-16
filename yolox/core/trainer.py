a8836dcc66c4a7b9ba0ba9e6caaea0a42034f289
Saurav <saurav.kdeo@gmail.com>
2023-04-16 09:57:26 -0400

    hf

--- yolox/core/trainer.py
+++ yolox/core/trainer.py
@@ -388,4 +388,4 @@ class Trainer:
                         "best_ap": self.best_ap,
                         "curr_ap": ap
                     }
-                )              
+                ) 
