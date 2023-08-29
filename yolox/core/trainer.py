4f08842c9537004af68f4b63c90bbf3ca5c0d875
beazy96 <bmaciolek@dtiq.com>
2023-08-29 10:49:59 +0200

    commit

--- yolox/core/trainer.py
+++ yolox/core/trainer.py
@@ -297,8 +297,7 @@ class Trainer:
         if self.iter + 1 == self.max_iter:
             loss_meter = self.meter.get_filtered_meter("loss")
             for k, v in loss_meter.items():
-                print(f"K: {k}, V: {v}")
-
+                mlflow.log_metric(k, v.latest, step=self.epoch)
             self.epoch_meter.clear_meters()
 
         # random resizing
