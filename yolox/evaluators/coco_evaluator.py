4accd72a357720f8c923a073f1d02dc40970c70a
beazy96 <bmaciolek@dtiq.com>
2023-08-29 16:35:44 +0200

    commit

--- yolox/evaluators/coco_evaluator.py
+++ yolox/evaluators/coco_evaluator.py
@@ -67,7 +67,7 @@ def per_class_AP_table(coco_eval, class_names=COCO_CLASSES, headers=["class", "A
         precision = precision[precision > -1]
         ap = np.mean(precision) if precision.size else float("nan")
         per_class_AP[name] = float(ap * 100)
-        mlflow.log_metric(f"AP_{name}", float(ap * 100))
+        mlflow.log_metric(f"AP_{name}", float(ap * 100), step=coco_eval.params.epoch)
 
     num_cols = min(colums, len(per_class_AP) * len(headers))
     result_pair = [x for pair in per_class_AP.items() for x in pair]
