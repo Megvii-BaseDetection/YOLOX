8e5735f8146a4ee54ca8c3b550352cb71d23de95
Jackson085 <jackson26594916@gmail.com>
2023-09-11 18:26:39 +0200

    Update train.py

--- tools/train.py
+++ tools/train.py
@@ -21,79 +21,21 @@ def make_parser():
     parser.add_argument("-n", "--name", type=str, default=None, help="model name")
 
     # distributed
-    parser.add_argument(
-        "--dist-backend", default="nccl", type=str, help="distributed backend"
-    )
-    parser.add_argument(
-        "--dist-url",
-        default=None,
-        type=str,
-        help="url used to set up distributed training",
-    )
+    parser.add_argument("--dist-backend", default="nccl", type=str, help="distributed backend")
+    parser.add_argument("--dist-url", default=None, type=str, help="url used to set up distributed training")
     parser.add_argument("-b", "--batch-size", type=int, default=64, help="batch size")
-    parser.add_argument(
-        "-d", "--devices", default=None, type=int, help="device for training"
-    )
-    parser.add_argument(
-        "-f",
-        "--exp_file",
-        default=None,
-        type=str,
-        help="plz input your experiment description file",
-    )
-    parser.add_argument(
-        "--resume", default=False, action="store_true", help="resume training"
-    )
+    parser.add_argument("-d", "--devices", default=None, type=int, help="device for training")
+    parser.add_argument("-f", "--exp_file", default=None, type=str, help="plz input your experiment description file")
+    parser.add_argument("--resume", default=False, action="store_true", help="resume training")
     parser.add_argument("-c", "--ckpt", default=None, type=str, help="checkpoint file")
-    parser.add_argument(
-        "-e",
-        "--start_epoch",
-        default=None,
-        type=int,
-        help="resume training start epoch",
-    )
-    parser.add_argument(
-        "--num_machines", default=1, type=int, help="num of node for training"
-    )
-    parser.add_argument(
-        "--machine_rank", default=0, type=int, help="node rank for multi-node training"
-    )
-    parser.add_argument(
-        "--fp16",
-        dest="fp16",
-        default=False,
-        action="store_true",
-        help="Adopting mix precision training.",
-    )
-    parser.add_argument(
-        "--cache",
-        type=str,
-        nargs="?",
-        const="ram",
-        help="Caching imgs to ram/disk for fast training.",
-    )
-    parser.add_argument(
-        "-o",
-        "--occupy",
-        dest="occupy",
-        default=False,
-        action="store_true",
-        help="occupy GPU memory first for training.",
-    )
-    parser.add_argument(
-        "-l",
-        "--logger",
-        type=str,
-        help="Logger to be used for metrics. \
-        Implemented loggers include `tensorboard` and `wandb`.",
-        default="tensorboard"
-    )
-    parser.add_argument(
-        "opts",
-        help="Modify config options using the command-line",
-        default=None,
-        nargs=argparse.REMAINDER,
-    )
+    parser.add_argument("-e", "--start_epoch", default=None, type=int, help="resume training start epoch")
+    parser.add_argument("--num_machines", default=1, type=int, help="num of node for training")
+    parser.add_argument("--machine_rank", default=0, type=int, help="node rank for multi-node training")
+    parser.add_argument("--fp16", dest="fp16", default=False, action="store_true", help="Adopting mix precision training.")
+    parser.add_argument("--cache", type=str, nargs="?", const="ram", help="Caching imgs to ram/disk for fast training.")
+    parser.add_argument("-o", "--occupy", dest="occupy", default=False, action="store_true", help="occupy GPU memory first for training.")
+    parser.add_argument("-l", "--logger", type=str, help="Logger to be used for metrics. \ Implemented loggers include `tensorboard` and `wandb`.", default="tensorboard")
+    parser.add_argument("opts",help="Modify config options using the command-line",default=None,nargs=argparse.REMAINDER)
     return parser
 
 
