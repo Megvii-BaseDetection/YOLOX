2c25d57f1ef9de09d538866862473175c17928df
Jackson085 <jackson26594916@gmail.com>
2023-10-24 16:23:53 +0200

    Update voc_classes.py

--- yolox/data/datasets/voc_classes.py
+++ yolox/data/datasets/voc_classes.py
@@ -5,4 +5,19 @@
 # VOC_CLASSES = ( '__background__', # always index 0
 VOC_CLASSES = (
     "drone",
+    "cat",
 )
+
+# def loadClassesAsTuple():
+#     classes = []
+#     with open('../../../datasets/VOCdevkit/VOC2012/labelmap.txt', 'r') as file:
+#         for line in file.readlines():
+#             classes.append(line.replace(" ", "").replace("\n", ""))
+#
+#     return tuple(classes)
+
+
+# VOC_CLASSES = loadClassesAsTuple()
+
+# if __name__ == '__main__':
+#     c = loadClassesAsTuple()
