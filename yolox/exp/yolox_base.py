53f2dfeaef8fb494dbdc4b3576044abdf92c9484
pkolebski <piotrkolebski@gmail.com>
2024-01-25 00:03:28 +0100

    fix loss creating

--- yolox/exp/yolox_base.py
+++ yolox/exp/yolox_base.py
@@ -123,7 +123,8 @@ class Exp(BaseExp):
         if getattr(self, "model", None) is None:
             in_channels = [256, 512, 1024]
             backbone = YOLOPAFPN(self.depth, self.width, in_channels=in_channels, act=self.act)
-            head = YOLOXHead(self.num_classes, self.width, in_channels=in_channels, act=self.act)
+            head = YOLOXHead(self.num_classes, self.width, in_channels=in_channels, act=self.act,
+                             iou_loss_type=self.iou_loss_type)
             self.model = YOLOX(backbone, head)
 
         self.model.apply(init_yolo)
