46824f7421e8256e2802ad5c2913c8ae991c3a26
beazy96 <bmaciolek@dtiq.com>
2024-07-23 14:09:57 +0200

    test

--- exps/default/yolox_s.py
+++ exps/default/yolox_s.py
@@ -13,3 +13,10 @@ class Exp(MyExp):
         self.depth = 0.33
         self.width = 0.50
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
+
+    def get_model(self):
+        from yolox.utils import freeze_module
+        model = super().get_model()
+        freeze_module(model.backbone.backbone)
+        print("Backbone is frozen.")
+        return model
