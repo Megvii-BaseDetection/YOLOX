b8ea10d31f81d1744e5ccf4533b63abb10241059
user <user@192.168.1.4>
2025-01-05 16:52:56 +0800

    Refactored train code.

--- tools/train.py
+++ tools/train.py
@@ -2,7 +2,9 @@
 # -*- coding:utf-8 -*-
 # Copyright (c) Megvii, Inc. and its affiliates.
 
-import argparse
+from pathlib import Path
+import yaml
+
 import random
 import warnings
 from loguru import logger
@@ -13,88 +15,6 @@ import torch.backends.cudnn as cudnn
 from yolox.core import launch
 from yolox.exp import Exp, check_exp_value, get_exp
 from yolox.utils import configure_module, configure_nccl, configure_omp, get_num_devices
-
-
-def make_parser():
-    parser = argparse.ArgumentParser("YOLOX train parser")
-    parser.add_argument("-expn", "--experiment-name", type=str, default=None)
-    parser.add_argument("-n", "--name", type=str, default=None, help="model name")
-
-    # distributed
-    parser.add_argument(
-        "--dist-backend", default="nccl", type=str, help="distributed backend"
-    )
-    parser.add_argument(
-        "--dist-url",
-        default=None,
-        type=str,
-        help="url used to set up distributed training",
-    )
-    parser.add_argument("-b", "--batch-size", type=int, default=64, help="batch size")
-    parser.add_argument(
-        "-d", "--devices", default=None, type=int, help="device for training"
-    )
-    parser.add_argument(
-        "-f",
-        "--exp_file",
-        default=None,
-        type=str,
-        help="plz input your experiment description file",
-    )
-    parser.add_argument(
-        "--resume", default=False, action="store_true", help="resume training"
-    )
-    parser.add_argument("-c", "--ckpt", default=None, type=str, help="checkpoint file")
-    parser.add_argument(
-        "-e",
-        "--start_epoch",
-        default=None,
-        type=int,
-        help="resume training start epoch",
-    )
-    parser.add_argument(
-        "--num_machines", default=1, type=int, help="num of node for training"
-    )
-    parser.add_argument(
-        "--machine_rank", default=0, type=int, help="node rank for multi-node training"
-    )
-    parser.add_argument(
-        "--fp16",
-        dest="fp16",
-        default=False,
-        action="store_true",
-        help="Adopting mix precision training.",
-    )
-    parser.add_argument(
-        "--cache",
-        type=str,
-        nargs="?",
-        const="ram",
-        help="Caching imgs to ram/disk for fast training.",
-    )
-    parser.add_argument(
-        "-o",
-        "--occupy",
-        dest="occupy",
-        default=False,
-        action="store_true",
-        help="occupy GPU memory first for training.",
-    )
-    parser.add_argument(
-        "-l",
-        "--logger",
-        type=str,
-        help="Logger to be used for metrics. \
-                Implemented loggers include `tensorboard`, `mlflow` and `wandb`.",
-        default="tensorboard"
-    )
-    parser.add_argument(
-        "opts",
-        help="Modify config options using the command-line",
-        default=None,
-        nargs=argparse.REMAINDER,
-    )
-    return parser
 
 
 @logger.catch
@@ -119,10 +39,16 @@ def main(exp: Exp, args):
 
 
 if __name__ == "__main__":
+    script_dir = Path(__file__).parent.resolve()
+
     configure_module()
-    args = make_parser().parse_args()
+
+    with open(script_dir / "train.yaml", "r") as f:
+        args = yaml.safe_load(f)
+    
     exp = get_exp(args.exp_file, args.name)
     exp.merge(args.opts)
+    
     check_exp_value(exp)
 
     if not args.experiment_name:
